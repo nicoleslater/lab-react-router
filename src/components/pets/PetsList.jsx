5d719bc788cd17e07985b7f298361835d8fbce89
Nicole Slater <nicoleslater@pursuit.org>
2023-08-14 18:42:19 -0400

    Submission

--- src/components/pets/PetsList.jsx
+++ src/components/pets/PetsList.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { Link, useParams } from "react-router-dom";
 
 export const PetsList = ({ pets }) => {
+  const params = useParams();
+  const { kind } = params
+  debugger
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -12,23 +16,27 @@ export const PetsList = ({ pets }) => {
     },
     [[], []]
   );
-
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
         {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+
+        {kind === "cats" &&
+          cats.map((cat) => <Pet key={cat.id} kind="cat" pet={cat} />)}
 
         {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        {kind === "dogs" &&
+          dogs.map((dog) => <Pet key={dog.id} kind="dog" pet={dog} />)}
+
+        {!kind &&
+          pets.map((pet) => (
+            <Pet key={pet.id} kind={pet.kind.toLowerCase()} pet={pet} />
+          ))}
       </section>
     </section>
   );
 };
 
 export default PetsList;
+
