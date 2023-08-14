a26e46325feeb525cfd521c1c0f45f1d40dbb445
Nicole Slater <nicoleslater@pursuit.org>
2023-08-14 18:32:41 -0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,8 @@ function App() {
       <Routes> 
       <Route path="/" element={<Home employees={employees} owners={owners} pets={pets} />} />
           <Route path="/staff" element={<StaffList employees={employees} />} />
-          <Route path="/cats/:id" element={<Pet pets={cats}  type={"Cats"} />} />
-          <Route path="/pets/cats/:id" element={<PetsList pets={pets} />} />
+          <Route path="/pets" element={<PetsList pets={pets} />} />
+          <Route path="/pets/:kind" element={<PetsList pets={pets} />} />
       </Routes> 
        
 
--- src/components/pets/PetsListNav.jsx
+++ src/components/pets/PetsListNav.jsx
@@ -5,16 +5,15 @@ export const PetsListNav = ({ cats, dogs }) => {
   return (
     <nav className="pets-nav">
       <ul>
-        <Link to={"/pets/cats"} element="cats" >
+       
         <li>
-          <a href="/pets/cats">See All Cats ({cats.length})</a>
+          <Link to="/pets/cats">See All Cats ({cats.length})</Link>
         </li>
-        </Link>
-        <Link to={"/pets/dogs"}> 
+     
         <li>
-          <a href="/pets/dogs">See All Dogs ({dogs.length})</a>
+          <Link to="/pets/dogs">See All Dogs ({dogs.length})</Link>
         </li>
-        </Link>
+       
       </ul>
     </nav>
   );
